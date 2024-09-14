a6ff74db57065303d696ebbd6dd46ac60e062497
DMC1290 <100431335+DMC1290@users.noreply.github.com>
2024-09-14 10:15:04 +0200

    debut

---
+++ monster.h
@@ -0,0 +1,11 @@
+#pragma once
+#
+
+struct Monster
+{
+
+private:
+
+
+
+};
--- tbs_game.cpp
+++ tbs_game.cpp
@@ -1,20 +1,53 @@
-// tbs_game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// steps:
+
+// 1 Text and visual
+
+// 2 Monsters
+
 
 #include <iostream>
+#include "monster.h"
+#include <string>
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Monster dragon("Mulaamnir");
+
+	std::cout << " Welcome to my first TBS Game !" << '\n';
+
+	std::cout << "." << '\n';
+
+	std::cout << " In this game there is 4 monsters." << '\n';
+
+	std::cout << "." << '\n';
+
+	std::cout << " You can chose 2 of them to see who wins in a fight." << '\n';
+
+	std::cout << "." << '\n';
+
+	std::cout << " 1 ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
